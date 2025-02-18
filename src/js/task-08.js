7ffd0399c80d00d8ef7083edc233e47a07aa6bb2
nata20019 <nbernikova68@gmail.com>
2025-02-18 10:14:12 +0100

    mod6jsn

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -4,18 +4,13 @@ form.addEventListener("submit", onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
-  input.forEach((item) => {
-    if (item.value === "") {
-      return alert("Всі поля повинні бути заповненні!");
-    }
-  });
   const formElements = event.currentTarget.elements;
   const email = formElements.email.value;
   const password = formElements.password.value;
-  if ((!email === "") & (!password === "")) {
-    const formData = { email, password };
-    console.log(formData);
+  if (!email || !password) {
+    return alert("Всі поля повинні бути заповненні!");
   }
-
+  const formData = { email, password };
+  console.log(formData);
   document.querySelector(".login-form").reset();
 }
